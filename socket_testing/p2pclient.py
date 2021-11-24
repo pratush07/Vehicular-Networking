fc5b17a6cf6ceb0d87f0548d3068cc3117ab4d46
Pratush Pandita <pratushpandita@Pratushs-MacBook-Pro.local>
2021-11-24 14:53:18 +0000

    socket testing

--- socket_testing/p2pclient.py
+++ socket_testing/p2pclient.py
@@ -5,23 +5,14 @@ import json
 
 def sendMessage():
     #Create a socket instance
-    socketObject = socket.socket()
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as socketObject:
+        socketObject = socket.socket()
 
-    #Using the socket connect to a server...in this case localhost
-    socketObject.connect(("localhost", 31000))
-    print("Connected to localhost")
-    msg = {"msg" : "hello!"}
+        #Using the socket connect to a server...in this case localhost
+        socketObject.connect(("localhost", 31000))
+        print("Connected to localhost")
+        msg = {"msg" : "hello!"}
 
-    socketObject.send(json.dumps(msg).encode('utf-8'))
+        socketObject.send(json.dumps(msg).encode('utf-8'))
 
-clients = []
-for i in range(5):
-    clients.append(Thread(target=sendMessage))
-
-for client in clients:
-    client.start()
-
-for client in clients:
-    client.join()
-
-
+sendMessage()
--- socket_testing/p2pserver.py
+++ socket_testing/p2pserver.py
@@ -1,19 +1,19 @@
 import socket
 
 # Create a server socket
-serverSocket = socket.socket()
-print("Server socket created")
-# Associate the server socket with the IP and Port
-ip      = "localhost"
-port    = 35491
-serverSocket.bind((ip, port))
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as serverSocket:
+    print("Server socket created")
+    # Associate the server socket with the IP and Port
+    ip      = "localhost"
+    port    = 35491
+    serverSocket.bind((ip, port))
 
-print("Server socket bound with with ip {} port {}".format(ip, port))
+    print("Server socket bound with with ip {} port {}".format(ip, port))
 
-# Make the server listen for incoming connections
-serverSocket.listen()
+    # Make the server listen for incoming connections
+    serverSocket.listen()
 
-while True:
-    (clientConnection, clientAddress) = serverSocket.accept()
-    data = clientConnection.recv(1024)
-    print(clientAddress, data)
+    while True:
+        (clientConnection, clientAddress) = serverSocket.accept()
+        data = clientConnection.recv(1024)
+        print(clientAddress, data)
