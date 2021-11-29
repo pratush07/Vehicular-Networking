954fd0f791a3856b4db6a50341af1b974efc333a
yasinz <yasinz@tcd.ie>
2021-11-29 18:09:02 +0000

    Adding sensors

---
+++ Sensors/LaneWidthSensor.py
@@ -0,0 +1,24 @@
+import json
+import random
+
+
+class Lanewidthsensor:
+
+    data = {}
+
+    def __init__(self, header):
+        self.header = header
+        self.generate_data()
+
+    def generate_data(self):
+        record_data = {'id': self.header, 'LaneWidth': random.uniform(3, 5)}
+        self.data = record_data
+
+    def get_data(self):
+        return self.data
+
+    def get_lane_width(self):
+        return self.data['LaneWidth']
+
+    def set_data(self, val):
+        self.data['LaneWidth'] = val
---
+++ Sensors/ProximitySensors.py
@@ -0,0 +1,25 @@
+import json
+import random
+
+
+class Proximitysensor:
+
+    data = {}
+
+    def __init__(self, header):
+        self.header = header
+        self.generate_data()
+
+    def generate_data(self):
+        record_data = {'id': self.header,
+                       'ObstacleDetected': False}
+        self.data = record_data
+
+    def get_data(self):
+        return self.data
+
+    def get_obstacle_distance(self):
+        return self.data['ObstacleDetected']
+
+    def set_data(self, val):
+        self.data['ObstacleDetected'] = val
