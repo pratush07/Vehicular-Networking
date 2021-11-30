2032a7a696f3de746f9d58625363170eae3efcd0
Pratush Pandita <pratushpandita@Pratushs-MacBook-Pro.local>
2021-11-30 13:48:39 +0000

    fixing typo

--- secure_comm.py
+++ secure_comm.py
@@ -15,7 +15,7 @@ def encrypt_public_key(a_message):
     print('encrypting ..' + str(a_message))
     enc_message = f.encrypt(a_message)
     print(enc_message)
-    return f.encrypt(enc_message)
+    return enc_message
 
 def decrypt_private_key(encoded_encrypted_msg):
     print('decrypting ..' + str(encoded_encrypted_msg))
